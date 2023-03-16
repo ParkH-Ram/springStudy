2659412b7bb99ef448d87d66adb49b24301997c8
ParkH-Ram <parkeryangga@gmail.com>
2023-03-16 19:27:22 +0900

    3-16 학교에서 스프링

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,14 +2,11 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>();  // 어딘가 저장..? 맵 ..
     private static long sequence = 0L;   // 실무에서는 동시성 문제 때문에 long 말고 다른것~
 
     @Override
@@ -20,20 +17,25 @@ public class MemoryMemberRepository implements MemberRepository{
     }
 
     @Override
-    public Optional<Member> frinById(Long id) {
+    public Optional<Member> findById(Long id) {
 
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));  // null 반환될 가능성이 있으면 Optional.ofNullable()로 감싸준다
     }
 
     @Override
     public Optional<Member> findByName(String name) {
+        return store.values().stream()  // 루프로 돌리겠다
+                .filter(member -> member.getName().equals(name))  // 멤버에서 멤버.겟네임이 name 파라미터로 넘어온 값하고 비교
+                .findAny();                                     // 하나라도 참으면 반환 (결과가 옵셔널로 반환) , 끝까지 돌렸는데 없으면 null 포함 되어 반환
 
-        return Optional.empty();
     }
 
     @Override
     public List<Member> findAll() {
 
-        return null;
+        return new ArrayList<>(store.values());  // store.values()  == members
+    }
+    public void clearStore(){
+        store.clear();   // 스토어를 싹 비우거든요~
     }
 }

84e221829d302300de94225d4e4602c9fd9989b4
Ram <parkeryangga@gmail.com>
2023-03-25 21:48:27 +0900

    23-3-25
    잠깐 테스트중
    근데 인텔리제이 왜 한글이 콘솔에 입력이 안되는고? 확인 바람

---
+++ hello-spring/src/main/java/hellostudy/happy.java
@@ -0,0 +1,13 @@
+package hellostudy;
+
+public class happy {
+    public static void main(String[]args){
+
+        int a=15;
+        int b=10;
+        System.out.println("합계" + a + b);
+
+
+    }
+
+}

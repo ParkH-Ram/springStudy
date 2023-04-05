ab818eba3b7688ec899699061710dd816e7002d1
ParkH-Ram <parkeryangga@gmail.com>
2023-04-05 14:17:54 +0900

    23-4-5 스프링 입문 - 코드로 배우는 스프링 부트 4장까지 전부 완료

--- springBoot/project2/src/main/java/com/shopping/project/controller/MemberController.java
+++ springBoot/project2/src/main/java/com/shopping/project/controller/MemberController.java
@@ -35,5 +35,14 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
 
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인");
+        return "/member/memberLoginForm";
+    }
 }

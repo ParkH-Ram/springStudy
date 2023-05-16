98648d5315e17c039b037effed6f611c32d79cc8
Ram <parkeryangga@gmail.com>
2023-05-16 18:16:43 +0900

    23-5-16  뭘 커밋하란겨?

---
+++ javaStudy/jpastudy/src/main/java/org/study/JpaMain.java
@@ -0,0 +1,12 @@
+package org.study;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaStudy");
+
+
+    }
+}

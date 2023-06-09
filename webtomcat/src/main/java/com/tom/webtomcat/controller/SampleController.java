f9d64087deee82cf5c96cd3e9395f849f5f96868
ParkH-Ram <parkeryangga@gmail.com>
2023-06-09 14:07:09 +0900

    23-6-9 코드로 배우는  웹 프로젝트

--- webtomcat/src/main/java/com/tom/webtomcat/controller/SampleController.java
+++ webtomcat/src/main/java/com/tom/webtomcat/controller/SampleController.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @Log4j
-@RequestMapping("/sample")
+@RequestMapping("/sample/*")
 public class SampleController {
 
 
@@ -22,20 +25,37 @@ public class SampleController {
         return "home";
     }
 
-    // 위럼 길게 쓰던걸 이 처럼 축약해서 사용 가능
+    // 위처럼 길게 쓰던걸 이 처럼 축약해서 사용 가능
     @GetMapping("/basicOnlyGet")
     public void basicGet2(){
         log.info("basic get only get.............");
     }
 
-
-    //6-3
-    @GetMapping("/ex1")
+    //6.3
+    @GetMapping("/ex1") //name=aaa&age=59
     public String ex1(SampleDTO dto){
         log.info("" + dto);
 
         return "ex1";
     }
 
+    //6.3.1
+    @GetMapping("/ex2") //name=aaa&age=59
+    public String ex2(@RequestParam("name") String name,
+                      @RequestParam("age") int age){
+        log.info("name : " + name);
+        log.info("age : " + age);
+
+        return "ex2";
+    }
+
+    //6.3.2
+    @GetMapping("/ex2List") // 배열로 처리  ?value=111&value=222&value=333
+    public String ex2List(@RequestParam("value")ArrayList<String> value){
+        log.info("value : " + value);
+
+        return "ex2List";
+
+    }
 
 }

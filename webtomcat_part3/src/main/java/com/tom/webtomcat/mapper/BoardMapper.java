5bd33c86139e37db2f363acd3749b14135959c32
ParkH-Ram <parkeryangga@gmail.com>
2023-06-16 16:41:54 +0900

    23 -6-16 CRUD 완료

--- webtomcat_part3/src/main/java/com/tom/webtomcat/mapper/BoardMapper.java
+++ webtomcat_part3/src/main/java/com/tom/webtomcat/mapper/BoardMapper.java
@@ -10,6 +10,16 @@ public interface BoardMapper {
 //    @Select("select * from tbl_board where bno > 0 ")
     public List<BoardVO> getList();
 
+    public void insert(BoardVO board);  // PK 값을 알 필요가 없는 경우
+
+    public void insertSelectKey(BoardVO board);  // PK 값을 알아야 하는 경우
+
+    public BoardVO read(Long bno);  // Read
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
+
 
 
 }
--- webtomcat_part3/src/test/java/com/tom/webtomcat/mapper/BoardMapperTest.java
+++ webtomcat_part3/src/test/java/com/tom/webtomcat/mapper/BoardMapperTest.java
@@ -1,5 +1,6 @@
 package com.tom.webtomcat.mapper;
 
+import com.tom.webtomcat.domain.BoardVO;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
@@ -7,6 +8,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {com.tom.webtomcat.config.RootConfig.class} )
@@ -23,6 +27,61 @@ public class BoardMapperTest {
         // BoardMapper에 지정된 메서드 호출
         // List<BoardVO> boardList = mapper. getList();
         // for (BoardVO board : boardList) { log info(board);
+    }
+
+    @Test
+    public void testInsert(){
+        BoardVO board = new BoardVO();
+        board.setTitle("새글");
+        board.setContent("새 내용");
+        board.setWriter("새 작성자");
+
+        mapper.insert(board);
+    }
+
+    @Test
+    public void testInsertSelectKey(){
+        BoardVO board = new BoardVO();
+        board.setTitle("셀렉 키 ");
+        board.setContent("셀렉 내용");
+        board.setWriter("셀랙 작성자");
+        mapper.insertSelectKey(board);
+    }
+
+    @Test
+    public void testRead(){
+        //존재하는 번호로 테스트
+        BoardVO board = mapper.read(21L);
+    }
+
+    @Test
+    public void  testDelete(){
+        int s = mapper.delete(22L);
+
+        if(s==0){
+            System.out.println( "값이 없습니다");
+        } else{
+            System.out.println("정상적으로 삭제 됨 ");
+        }
+
+    }
+
+    @Test
+    public void testUpdate(){
+        BoardVO board = new BoardVO();
+        List<BoardVO> boardList = new ArrayList<>();
+        // 실행전 존재하는 번호 확인
+
+
+        board.setBno(2L);
+        board.setTitle("수정된 제목");
+        board.setContent("수정된 내용");
+        board.setWriter("수정된 유저");
+
+
+
+        int count = mapper.update(board);
+
     }
 
 

10218dd103cd537eaa3e8ed5a5f58321c736571f
ParkH-Ram <parkeryangga@gmail.com>
2023-06-16 10:37:40 +0900

    23-6-16 갑자기 mapper가 된다. 결국 오타였나?

--- webtomcat_part3/src/main/java/com/tom/webtomcat/mapper/BoardMapper.java
+++ webtomcat_part3/src/main/java/com/tom/webtomcat/mapper/BoardMapper.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public interface BoardMapper {
 
-    @Select("select * from tbl_board where bno > 0 ")
+//    @Select("select * from tbl_board where bno > 0 ")
     public List<BoardVO> getList();
+
+
 
 }

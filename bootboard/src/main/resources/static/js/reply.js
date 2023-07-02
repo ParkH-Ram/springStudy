4a9f152f98bc1ba696d9a9ef06bdd7260dc7ea1d
Ram <parkeryangga@gmail.com>
2023-07-02 13:19:20 +0900

    23 - 7 - 2 댓글 목록 근데 페이지 네이션 안되는거 같은디

--- bootboard/src/main/resources/static/js/reply.js
+++ bootboard/src/main/resources/static/js/reply.js
@@ -1,15 +1,24 @@
 async function get1(bno){
     const result = await axios.get(`/replies/list/${bno}`)
-    console.log(result)
+    
+    // console.log(result)
+    return result;
 
 }
 
-// async function getList({bno, page, size, goLast}){
-//
-//     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
-//
-//     return result.data
-// }
+async function getList({bno, page, size, goLast}){
+
+    const result = await axios.get(`/replies/list/${bno}`, { params: {page, size} })
+
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total / size))
+        return getList({bno:bno,page:lastPage, size:size})
+
+    }
+
+    return result.data
+}
 
 
 /*

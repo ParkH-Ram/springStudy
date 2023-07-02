7de02d12962a9e2a4f69d2d5694270cd97a6c596
Ram <parkeryangga@gmail.com>
2023-07-02 17:50:18 +0900

    23-7-2 댓글 모달창 add 까지 완료 백업 도 완료

--- bootboard/src/main/resources/static/js/reply.js
+++ bootboard/src/main/resources/static/js/reply.js
@@ -20,6 +20,11 @@ async function getList({bno, page, size, goLast}){
     return result.data
 }
 
+async function addReply(replyObj){
+    const response = await axios.post(`/replies/`, replyObj)
+    return response.data
+}
+
 
 /*
 async function getList({bno, page, size, goLast}){

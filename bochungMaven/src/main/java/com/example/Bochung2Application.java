9ce7cee33507f191245e068fbc3a1a830e41d459
Ram <parkeryangga@gmail.com>
2023-07-19 22:36:45 +0900

    23-7-19 ----

---
+++ bochungMaven/src/main/java/com/example/Bochung2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bochung2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bochung2Application.class, args);
+	}
+
+}

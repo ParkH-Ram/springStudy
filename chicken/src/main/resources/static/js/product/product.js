06355517684196e88e328a4e8b760b417a3f324c
ParkH-Ram <parkeryangga@gmail.com>
2023-11-09 12:11:28 +0900

    23-11-9 오홍 계산기

--- chicken/src/main/resources/static/js/product/product.js
+++ chicken/src/main/resources/static/js/product/product.js
@@ -15,9 +15,11 @@ function deleteProduct(obj){
 }
 
 // 치킨 계산기
-function chickenCal1(){
-
+function chickenCal1() {
+    // 모달 보이기
+    document.getElementById('proteinModal').style.display = "block";
 }
+
 
 function chickenCal1_2(){
 
@@ -31,3 +33,19 @@ function chickenCal2(){
 
 }
 
+let multiplier = 1;
+let productProtein = document.querySelector('button[data-toggle="modal"]').dataset.protein;
+
+function setMultiplier(value) {
+    multiplier = value;
+}
+
+function calculateProtein() {
+    let weight = document.getElementById('weight').value;
+    let neededProtein = weight * multiplier;
+    let neededProduct = Math.ceil(neededProtein / productProtein);
+
+    document.getElementById('result').innerText =
+        '당신의 몸무게에 필요한 단백질량은 ' + neededProtein + 'g 입니다. \n' +
+        ' 이를 충족하기 위해선 해당 제품을 ' + neededProduct + '개 먹어야 합니다.';
+}

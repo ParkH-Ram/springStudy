fd0744f1f0bb4dfd586ad6be40cbb8196b203c8f
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-11-18 20:07:51 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,10 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
         
+        // 정수의 길이 만큼 배열 생성
         int [] answer = new int [n];
         
+        // 정수의 길이 만큼 배열 반복
         for(int i = 0; i < n; i++){
             
+            // 리턴할 배열 인덱스에 입력받은 배열 인덱스에 맞춰 값들 넣어주기 
             answer[i] = num_list[i];
         }
         

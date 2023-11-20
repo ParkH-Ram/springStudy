4b463e504eef3197cf282fa8983ec008a0a6fe22
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-11-20 10:40:29 +0900

    [level unrated] Title: 배열의 평균값, Time: 2.41 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -1,11 +1,9 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] numbers) {
-        double answer = 0;
-        
-        for(int i : numbers){
-            answer += i;
-        }
-        
-        return answer / numbers.length;
+   
+              // 스트림 -> 스트림(배열).(연산할 것 넣고).(0이 아니면 값을, 0이면 0 리턴 )
+        return Arrays.stream(numbers).average().orElse(0);
     }
 }

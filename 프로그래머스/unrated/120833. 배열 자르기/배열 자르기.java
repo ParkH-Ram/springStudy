0b9c32dfb1fd05e6ca3ab5dae21cdd117ed41ad4
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-12-02 14:32:18 +0900

    [level unrated] Title: 배열 자르기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
+        // +1 하는 이유는  copyOfRange는 시작 인덱스 부터 끝 인덱스 까지 배열을 복사
+        //  즉 num1 = 1, num2 = 3 이면 1~3  3번 앞 인덱스 까지 인데 
+        //  문제에서 요구하는 것은 3번 인덱스 까지 출력 해야 하기 때문에 
         return Arrays.copyOfRange(numbers, num1, num2+1);
     }
 }

31c33afa91302b8d7817e499501b62417c458f1c
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-12-08 14:36:29 +0900

    [level unrated] Title: 개미 군단, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -1,23 +1,12 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
         int valueA = hp % 5;
-        int valueB = hp % 3;
         
         int a = hp / 5; // 제일 먼저 장군으로 피를 깎고
         int b = valueA / 3;  // 피를 깎고 남은 hp를  병정으로 깎고 
-        int c = b / 1  ;  // 마지막 남은 hp를 일개미가 깎는다 
-        
-        System.out.println(valueA);
-        
-        if(valueA % 3 == 0) c  = 0;
-        else if(valueA == 1 ) c = 1;
-        else if(valueA == 2 ) c = 2;
-        
-        
-                
-        
-          
+        int c = valueA % 3;  // 마지막 남은 hp를 일개미가 깎는다 
+
+
         return a+b+c;
     }
 }

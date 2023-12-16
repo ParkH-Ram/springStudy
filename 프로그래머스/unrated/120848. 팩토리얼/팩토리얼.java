4958677df37f10c1253307581adae7ec17ae2e4c
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-12-16 20:06:34 +0900

    [level unrated] Title: 팩토리얼, Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/unrated/120848. 팩토리얼/팩토리얼.java
@@ -3,22 +3,15 @@ class Solution {
         
         int sum = 1;
         int max = 0;
-        int total = 0;
         
         for(int i=1; i<=10; i++){
         
             sum *= i;
-            
-            
-            System.out.println(sum);
-            System.out.println(total);
+    
             if(sum == n) max = i;
             else if(sum < n) max = i;
-            
-            
-            
+               
         }
-        System.out.println(n);
 
         return max;
     }

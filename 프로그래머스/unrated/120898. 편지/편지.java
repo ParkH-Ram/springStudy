e252ea2c7c1e27567295a98f3cd2a6d8e31690cc
Ram <119924003+ParkH-Ram@users.noreply.github.com>
2023-12-17 22:16:32 +0900

    [level unrated] Title: 편지, Time: 0.02 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120898. 편지/편지.java
+++ 프로그래머스/unrated/120898. 편지/편지.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
         
-        if (message.length() * 2 <= 22) answer = 22;
-        else answer = message.length() * 2;
-        
-        return answer;
+        return message.length() * 2;
     }
 }

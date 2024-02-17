dba4118f14a13b8a31a89593feac8ffb2230a1af
Ram <parkeryangga@gmail.com>
2024-02-18 07:16:36 +0900

    todo 만들기 저장 2

--- todoList2/app/src/main/java/com/first/todolist2/DBHelper.java
+++ todoList2/app/src/main/java/com/first/todolist2/DBHelper.java
@@ -1,9 +1,12 @@
 package com.first.todolist2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -32,9 +35,51 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    // INSERT 문
+    // select ( 조회 )
+    public ArrayList<TodoItem> getTodoList(){
+        ArrayList<TodoItem> todoItems = new ArrayList<>();
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM TodoList ORDER BY writeDate DESC", null);
+
+        // 조회 했을 때 데이터가 있을 때 내부 수행
+        if(cursor.getCount() != 0){
+            while(cursor.moveToNext()){
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String title = cursor.getString(cursor.getColumnIndex("title"));
+                String content = cursor.getString(cursor.getColumnIndex("content"));
+                String writeDate = cursor.getString(cursor.getColumnIndex("writeDate"));
+
+                TodoItem todoItem = new TodoItem();
+                todoItem.setId(id);
+                todoItem.setTitle(title);
+                todoItem.setContent(content);
+                todoItem.setWriteDate(writeDate);
+                todoItems.add(todoItem) ;
+            }
+        }
+        cursor.close();
+        return todoItems;
+
+    }
+
+
+    // insert 문 ( 삽입 )
     public void insertTodo(String title, String content, String writeDate){
         SQLiteDatabase db = getWritableDatabase();
-        db.execSQL("INSERT INTO TodoList (title, content, writeDate) VALUES()");
+        db.execSQL("INSERT INTO TodoList (title, content, writeDate) " +
+            "VALUES('" + title +"', '" + content +"', '" + writeDate +"'); ");
+    }
+
+    // update 문 ( 수정 )
+    public void updateTodo(String title, String content, String writeDate, String beforeDate){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE TodoList SET title='" + title +"', content='" + content +"', writeDate='" + writeDate +"' WHERE writeDate='"+ beforeDate +"'");
+    }
+
+    // delete 문 ( 삭제 )
+    public void deleteTodo(String beforeDate){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM TodoList WHERE writeDate = '" + beforeDate + "'");
     }
 }
--- todoList2/app/src/main/java/com/first/todolist2/MainActivity.java
+++ todoList2/app/src/main/java/com/first/todolist2/MainActivity.java
@@ -1,13 +1,67 @@
 package com.first.todolist2;
 
+import android.app.Dialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    // m- > 전역변수 앞에 붙임 구분 위해 사용
+    private RecyclerView mRv_todo;
+    private FloatingActionButton mBtn_write;
+    private ArrayList<TodoItem> mTodoItems;
+    private DBHelper mDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setInit(){
+
+        mDBHelper = new DBHelper(this);
+        mRv_todo = findViewById(R.id.rv_todo);
+        mBtn_write = findViewById(R.id.btn_write);
+        mTodoItems = new ArrayList<>();
+
+        mBtn_write.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                // 팝업창 띄우기
+                Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Material_Light_Dialog);
+                dialog.setContentView(R.layout.dialog_edit);
+
+                // 뷰가 연결 됐다?
+                EditText et_title = dialog.findViewById(R.id.et_title);
+                EditText et_content = dialog.findViewById(R.id.et_content);
+                Button btn_ok = dialog.findViewById(R.id.btn_ok);
+                btn_ok.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+
+                        // insert Database
+                        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()); // 현재시간 포맷에 맞춰 받아온 뒤 스트링에 넣는다
+                        mDBHelper.insertTodo(et_title.getText().toString(), et_content.getText().toString(), currentTime);
+
+                        // insert Ui
+
+                    }
+                });
+                dialog.show();
+            }
+        });
+
     }
 }
